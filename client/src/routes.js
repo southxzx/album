0c19c380d7989f2605a39cfc201b6da570903bdd
southxz <southxzx109@users.noreply.github.com>
2020-08-27 16:07:31 +0700

    sdfg

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './component/Home';
 import Layout from './hoc/layout';
+import RegisterLogin from './component/Register_login';
+
 
 const Routes = () => {
   return(
     <Layout>
       <Switch>
+        <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Layout>
