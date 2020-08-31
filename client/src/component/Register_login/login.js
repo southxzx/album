b9b59ecd19ac0a6a9ec8bbae5900d4e47044a848
southxz <southxzx109@users.noreply.github.com>
2020-08-31 11:29:39 +0700

    c

--- client/src/component/Register_login/login.js
+++ client/src/component/Register_login/login.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import FormField from '../utils/Form/formfield';
 import { update, generateData, isFormValid } from '../utils/Form/formActions';
+import { loginUser } from '../../redux/actions/user_actions';
+import { withRouter } from 'react-router-dom';
+
 
 class Login extends Component {
 
@@ -51,7 +54,18 @@ class Login extends Component {
         let formIsValid = isFormValid(this.state.formdata,'login');
 
         if (formIsValid){
-            console.log(dataToSubmit);
+            // Dispatch (bên trong có action trả về type + payload)
+            // Dispatch sẽ thực thi Reducer
+            this.props.dispatch(loginUser(dataToSubmit)).then(response => {
+                if (response.payload.loginSuccess){
+                    console.log(response.payload);
+                    this.props.history.push('user/dashboard')
+                }else{
+                    this.setState({
+                        formError: true
+                    })
+                }
+            });
         } else {
             this.setState({
                 formError: true
@@ -103,4 +117,4 @@ class Login extends Component {
     }
 }
 
-export default connect()(Login);
+export default connect()(withRouter(Login));
